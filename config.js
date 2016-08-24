526963c5e7fc570eb73d25b1379c6543a26146fb
Michael ODonnell <modonnell@grassroots-tech.com>
2016-08-24 17:57:14 -0400

    fixing chunked data accumulation

--- config.js
+++ config.js
@@ -5,7 +5,9 @@ var config = {};
 
 config.proxy_port = 8080;
 
-config.target_host = "demo8978876.mockable.io";
+config.target_host = "www.xkcd.com";
+//config.target_host = "www.google.com";
+//config.target_host = "demo8978876.mockable.io";
 config.target_port = 80;
 
 module.exports = config;
